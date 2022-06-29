c3dc792050e93c9c9e7b227d7e638fa1520bed21
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2022-06-29 11:45:01 +0200

    Remove unnecessary files, add GlobalStyle, add favicon

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,5 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+
+* {
+    box-sizing: border-box;
+}
+
 body {
-  margin: 0;
+  margin: 0 auto;
+  max-width: 600px;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
@@ -11,3 +20,5 @@ code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
 }
+
+`;
