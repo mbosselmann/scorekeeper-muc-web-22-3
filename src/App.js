95d36be8e171474fb4a54ab984536bf6ff1efaf6
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2022-06-30 10:17:41 +0200

    Add routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Player from "./components/Player/Player.js";
 import Button from "./components/Button/Button.js";
 import History from "./components/History/History.js";
 import { initialPreviousGames } from "./dbHistory.js";
+import { Route, Routes } from "react-router-dom";
 
 const players = [
   {
@@ -34,17 +35,37 @@ function App() {
         <h1>Scorekeeper</h1>
       </header>
       <Main>
-        <h2>Game</h2>
-        {players.map((player) => {
-          return (
-            <Player key={player.id} name={player.name} score={player.score} />
-          );
-        })}
-        <Button background={"lightseagreen"}>end game</Button>
-        <h2>Previous Games</h2>
-        {initialPreviousGames.map((game) => {
-          return <History key={game.id} game={game} />;
-        })}
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <h2>Game</h2>
+                {players.map((player) => {
+                  return (
+                    <Player
+                      key={player.id}
+                      name={player.name}
+                      score={player.score}
+                    />
+                  );
+                })}
+                <Button background={"lightseagreen"}>end game</Button>
+              </>
+            }
+          />
+          <Route
+            path="/history"
+            element={
+              <>
+                <h2>Previous Games</h2>
+                {initialPreviousGames.map((game) => {
+                  return <History key={game.id} game={game} />;
+                })}
+              </>
+            }
+          />
+        </Routes>
       </Main>
     </>
   );
